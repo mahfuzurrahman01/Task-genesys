8cf2efb4c8bedbbce9d6cfef55522cab39f9dded
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2023-02-11 01:36:48 +0600

    .

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
-
+import banner from '../assets/images/banner.jpg'
+import image1 from '../assets/images/Rectangle 16.png'
+import image2 from '../assets/images/Rectangle 17.png'
+import image3 from '../assets/images/Rectangle 20.png'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 const Home = () => {
+
     return (
         <div>
-            <h1>this is home</h1>
+            <div className='relative'>
+                <img src={banner} alt="" className='' />
+                <div className='absolute  lg:bottom-28 md:bottom-10 bottom-8 left-10 lg:left-24 md:left-9 flex flex-col lg:gap-6 md:gap-4 gap-3 items-start'>
+                    <h1 className='font-thin lg:text-5xl md:text-3xl text-white'>Live up to your expectation <br /> The true delight for your space.</h1>
+                    <p className='uppercase text-white lg:text-xl md:text-lg md:block hidden font-light'>Your sense of comfort redefined</p>
+                    <button className='md:px-10 px-3 md:py-3 py-1 md:bg-opacity-100 bg-opacity-80 text-white hover:bg-opacity-25' style={{ backgroundColor: "#BCB2A0" }}>SHOP NOW</button>
+                </div>
+            </div>
+            <div style={{ backgroundColor: "#F0F0F0" }}>
+                <div className='w-5/6 mx-auto py-10'>
+                    <h1 className='text-2xl font-light mt-10'>OUR COLLECTION</h1>
+                    <div className='mt-8 flex justify-between'>
+                        {/* ========================= card1 =========================== */}
+                        <div data-aos="fade-left" className="w-[30%] text-gray-700 hover:shadow-xl hover:scale-105  hover:shadow-gray-300">
+                            <img src={image1} alt="" className="w-full max-h-96" />
+                            <div className="mt-6 pb-3">
+                                <p className="uppercase text-xl text-center">Flower</p>
+                                <p className="text-gray-700 text-center font-light text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, magni.</p>
+                            </div>
+                        </div>
+                        {/* ========================= card2 =========================== */}
+                        <div data-aos="fade-up" className="w-[30%] text-gray-700 hover:shadow-xl hover:scale-105  hover:shadow-gray-300">
+                            <img src={image2} alt="" className="w-full max-h-96" />
+                            <div className="mt-6 pb-3">
+                                <p className="uppercase text-xl text-center">Candles</p>
+                                <p className="text-gray-700 text-center font-light text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, magni.</p>
+                            </div>
+                        </div>
+                        {/* ========================= card3 =========================== */}
+                        <div data-aos="fade-right" className="w-[30%] text-gray-700 hover:shadow-xl hover:scale-105  hover:shadow-gray-300">
+                            <img src={image3} alt="" className="w-full max-h-96" />
+                            <div className="mt-6 pb-3">
+                                <p className="uppercase text-xl text-center">Lamp Shade</p>
+                                <p className="text-gray-700 text-center font-light text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, magni.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,30 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo/LOGO.png'
-import {CiSearch, CiShoppingCart, CiUser} from 'react-icons/ci'
+import { CiSearch, CiShoppingCart, CiUser } from 'react-icons/ci'
+import { SlMenu } from 'react-icons/sl'
+import { HiOutlineXMark } from 'react-icons/hi2'
 const Navbar = () => {
-    return (
-        <header className=" bg-gray-100 text-gray-700" >
-	<div className="px-16 flex justify-between mx-auto items-center py-5">
-		<ul className="flex justify-start items-center gap-5 overflow-auto w-[33%]">
-		<NavLink to='/' className={({isActive}) => isActive ? "border-b-2 hover:rounded border-orange-200 hover:border-b-2 hover:border-gray-200 ":"" }><li className='hover:bg-gradient-to-l hover:bg-orange-200 duration-300 hover:from-gray-200 py-1 px-3 rounded'>Home</li></NavLink>	
-		<NavLink to='/shop' className={({isActive}) => isActive ? "border-b-2 hover:rounded border-orange-200 hover:border-b-2 hover:border-gray-200  ":"" }><li className='hover:bg-gradient-to-l hover:bg-orange-200 duration-300 hover:from-gray-200 py-1 px-3 rounded'>Shop</li></NavLink>	
-		<NavLink to='/about' className={({isActive}) => isActive ? "border-b-2 hover:rounded border-orange-200 hover:border-b-2 hover:border-gray-200  ":"" }><li className='hover:bg-gradient-to-l hover:bg-orange-200 duration-300 hover:from-gray-200 py-1 px-3 rounded'>About Us</li></NavLink>	
-		<NavLink to='/contact' className={({isActive}) => isActive ? "border-b-2 hover:rounded border-orange-200 hover:border-b-2 hover:border-gray-200  ":"" }><li className='hover:bg-gradient-to-l hover:bg-orange-200 duration-300 hover:from-gray-200 py-1 px-3 rounded'>Contact us</li></NavLink>	
-		</ul>
+	const [show, setShow] = useState(false)
 
-       <div className='w-[33%] flex justify-center'>
-	   <img src={logo} alt="logo" className='w-16 h-16' />
-	   </div>
-		
-		<div className="flex items-center gap-4 justify-end w-[33%] pr-8">
-			<CiSearch className='w-6 h-6 text-gray-700'></CiSearch>
-			<CiUser className='w-6 h-6 text-gray-700'></CiUser>
-			<CiShoppingCart className='w-6 h-6 text-gray-700'></CiShoppingCart>
+	return (
+		<div className='sticky top-0 z-20'>
+			{/* =============================desktop version navbar ============================ */}
+			<header className=" bg-gray-100 text-gray-700 opacity-90 lg:block hidden" >
+				<div className="px-16 flex justify-between mx-auto items-center py-5">
+					<ul className="flex justify-start items-center gap-5 overflow-auto w-[33%]">
+						<NavLink to='/' className={({ isActive }) => isActive ? "border-b-2 hover:rounded border-orange-200 hover:border-b-2 hover:border-gray-200 " : ""}><li className='hover:bg-gradient-to-l hover:bg-orange-200 duration-300 hover:from-gray-200 py-1 px-3 rounded'>Home</li></NavLink>
+						<NavLink to='/shop' className={({ isActive }) => isActive ? "border-b-2 hover:rounded border-orange-200 hover:border-b-2 hover:border-gray-200  " : ""}><li className='hover:bg-gradient-to-l hover:bg-orange-200 duration-300 hover:from-gray-200 py-1 px-3 rounded'>Shop</li></NavLink>
+						<NavLink to='/about' className={({ isActive }) => isActive ? "border-b-2 hover:rounded border-orange-200 hover:border-b-2 hover:border-gray-200  " : ""}><li className='hover:bg-gradient-to-l hover:bg-orange-200 duration-300 hover:from-gray-200 py-1 px-3 rounded'>About Us</li></NavLink>
+						<NavLink to='/contact' className={({ isActive }) => isActive ? "border-b-2 hover:rounded border-orange-200 hover:border-b-2 hover:border-gray-200  " : ""}><li className='hover:bg-gradient-to-l hover:bg-orange-200 duration-300 hover:from-gray-200 py-1 px-3 rounded'>Contact us</li></NavLink>
+					</ul>
+
+					<div className='w-[33%] flex justify-center'>
+						<img src={logo} alt="logo" className='w-16 h-16' />
+					</div>
+
+					<div className="flex items-center gap-4 justify-end w-[33%] pr-8">
+						<CiSearch className='w-6 h-6 text-gray-700'></CiSearch>
+						<CiUser className='w-6 h-6 text-gray-700'></CiUser>
+						<CiShoppingCart className='w-6 h-6 text-gray-700'></CiShoppingCart>
+					</div>
+				</div>
+			</header>
+			{/* =============================mobile and tablet version Navbar============================ */}
+			<div className=" bg-gray-100 text-gray-700 lg:hidden block">
+				<div className='py-2 px-4 flex justify-between items-center'>
+					<div>
+						<img src={logo} className="w-10 h-10 md:w-16 md:h-16" alt="" />
+					</div>
+					<div onClick={() => setShow(!show)}>
+						{
+
+							show ? <HiOutlineXMark className="w-7 h-7 md:w-9 md:h-9 text-gray-500"></HiOutlineXMark> :
+								<SlMenu className="w-6 h-6 md:w-8 md:h-8 text-gray-500"></SlMenu>
+
+						}
+					</div>
+				</div>
+				<div >
+
+					<ul className={`flex justify-center items-center md:gap-10 gap-2 absolute ${show ? 'md:translate-y-[-50px] translate-y-[-40px]' : 'translate-y-[-340px]'} duration-300 left-20 md:left-52`}>
+						<NavLink to='/' className={({ isActive }) => isActive ? "border-b-2 hover:rounded border-orange-200 hover:border-b-2 hover:border-gray-200 " : ""}><li className='hover:bg-gradient-to-l hover:bg-orange-200 duration-300 hover:from-gray-200 py-1 md:px-3 px-1 rounded'>Home</li></NavLink>
+						<NavLink to='/shop' className={({ isActive }) => isActive ? "border-b-2 hover:rounded border-orange-200 hover:border-b-2 hover:border-gray-200  " : ""}><li className='hover:bg-gradient-to-l hover:bg-orange-200 duration-300 hover:from-gray-200 py-1 md:px-3 px-1 rounded'>Shop</li></NavLink>
+						<NavLink to='/about' className={({ isActive }) => isActive ? "border-b-2 hover:rounded border-orange-200 hover:border-b-2 hover:border-gray-200  " : ""}><li className='hover:bg-gradient-to-l hover:bg-orange-200 duration-300 hover:from-gray-200 py-1 md:px-3 px-1 rounded'>About Us</li></NavLink>
+						<NavLink to='/contact' className={({ isActive }) => isActive ? "border-b-2 hover:rounded border-orange-200 hover:border-b-2 hover:border-gray-200  " : ""}><li className='hover:bg-gradient-to-l hover:bg-orange-200 duration-300 hover:from-gray-200 py-1 md:px-3 px-1 rounded'>Contact us</li></NavLink>
+					</ul>
+
+
+				</div>
+			</div>
+
 		</div>
-	</div>
-</header>
-    );
+	);
 };
 
 export default Navbar;
